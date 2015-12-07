8974a937121400b7f39ac415839f2d16ec223fc2
Orange1861 <orange1861.65@gmail.com>
2015-12-07 16:46:22 +0100

    Create Reference.java

---
+++ main/java/orangeVillager61.Orania/Reference.java
@@ -0,0 +1,9 @@
+package orangeVillager61.Orania;
+
+public class Reference {
+	public static final String MOD_ID = "OA";
+	public static final String MOD_NAME = "Orania";
+	public static final String VERSION = "0.1";
+	public static final String CLIENT_PROXY_CLASS = "orangeVillager61.Orania.proxy.ClientProxy";
+	public static final String SERVER_PROXY_CLASS = "orangeVillager61.Orania.proxy.CommonProxy";
+}
