e07c17f561f0b391c94559d4cf9d158ae4d92c52
Orange1861 <orange1861.65@gmail.com>
2015-12-08 10:16:53 +0100

    Create IMetaBlockName.java

---
+++ main/java/orangeVillager61.Orania/blocks/IMetaBlockName.java
@@ -0,0 +1,10 @@
+ackage orangeVillager61.Orania.blocks;
+
+import net.minecraft.item.ItemStack;
+
+public interface IMetaBlockName {
+
+        String getSpecialName(ItemStack stack); 
+        String getSpecialName1(ItemStack stack);
+        
+}
